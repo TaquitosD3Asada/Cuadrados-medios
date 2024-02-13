6df9dbd51e320c83d17992b80c4d80e027516349
TaquitosD3Asada <Haloreba117@gmail.com>
2024-02-12 19:04:46 -0600

    Primera Actualización

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,71 @@
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const btnGenerar = document.getElementById('btnGenerar');
-    const inputSemilla = document.getElementById('semilla');
+    const btnReiniciar = document.getElementById('btnReiniciar');
+    const inputSemilla1 = document.getElementById('semilla1');
+    const inputSemilla2 = document.getElementById('semilla2');
     const inputIteraciones = document.getElementById('iteraciones');
     const resultado = document.getElementById('resultado');
 
-    // Función para generar números pseudoaleatorios con el algoritmo de cuadrados medios
-    function cuadradosMedios(semilla, iteraciones) {
+    function productosMedios(semilla1, semilla2, iteraciones) {
         let resultadoTexto = '';
 
         for (let i = 0; i < iteraciones; i++) {
-            // Verificar que la semilla tenga al menos 4 dígitos
-            if (semilla.toString().length < 3) {
-                alert('Ingrese una semilla válida con al menos 4 dígitos.');
-                document.querySelector('label[for="semilla"]').textContent = 'Semilla Inicial:';
-                inputSemilla.value = '';
+            if (semilla1.toString().length < 3 || semilla2.toString().length < 3) {
+                alert('Ingrese semillas válidas con al menos 4 dígitos.');
+                inputSemilla1.value = '';
+                inputSemilla2.value = '';
                 inputIteraciones.value = '';
                 return;
             }
 
-            // Obtener el cuadrado de la semilla
-            let cuadrado = semilla * semilla;
+            let producto = semilla1 * semilla2;
+            let productoStr = producto.toString();
 
-            // Convertir el cuadrado a una cadena de texto
-            let cuadradoStr = cuadrado.toString();
-
-            // Verificar si la longitud es impar
-            if (cuadradoStr.length % 2 !== 0) {
-                // Si es impar, agregar un cero a la izquierda
-                cuadradoStr = '0' + cuadradoStr;
+            if (productoStr.length % 2 !== 0) {
+                productoStr = '0' + productoStr;
             }
 
-            // Obtener los 4 dígitos centrales como nueva semilla utilizando la función
-            let nuevaSemilla = obtenerDigitosCentrales(cuadradoStr);
-            
-            // Agregar el número pseudoaleatorio al resultado
-            resultadoTexto += `${i + 1}: 0.${nuevaSemilla}\n`;
+            let nuevaSemilla = obtenerDigitosCentrales(productoStr);
+            resultadoTexto += `R${i + 1}: 0.${nuevaSemilla}\n`;
 
-            // Actualizar la semilla para la próxima iteración
-            semilla = parseInt(nuevaSemilla);
+            semilla1 = semilla2;
+            semilla2 = parseInt(nuevaSemilla);
         }
 
-        return resultadoTexto; // Devolver el resultado en lugar de asignarlo directamente
+        return resultadoTexto;
     }
 
-    // Función para manejar el evento de click en el botón generar
     function generarNumeros() {
-        const semilla = parseInt(inputSemilla.value);
+        const semilla1 = parseInt(inputSemilla1.value);
+        const semilla2 = parseInt(inputSemilla2.value);
         const iteraciones = parseInt(inputIteraciones.value);
 
-        if (isNaN(semilla) || isNaN(iteraciones) || semilla <= 0 || iteraciones <= 0) {
-            alert('El valor maximo de iteraciones es 106.');
+        if (isNaN(semilla1) || isNaN(semilla2) || isNaN(iteraciones) || semilla1 <= 0 || semilla2 <= 0 || iteraciones <= 0) {
+            alert('Ingrese valores válidos para las semillas (mayor o igual a 4) e iteraciones (mayor a 0).');
             return;
         }
 
-        const resultadoTexto = cuadradosMedios(semilla, iteraciones);
+        const resultadoTexto = productosMedios(semilla1, semilla2, iteraciones);
         resultado.textContent = resultadoTexto;
     }
 
     btnGenerar.addEventListener('click', generarNumeros);
-
-    // Función para obtener los dígitos centrales de una cadena
-    function obtenerDigitosCentrales(cadena) {
-        // Obtener la longitud de la cadena
-        let longitud = cadena.length;
     
-        // Calcular los índices del primer y último dígito central
+    function reiniciarDatos() {
+        inputSemilla1.value = ''; // Limpiar el valor del input de la primera semilla
+        inputSemilla2.value = ''; // Limpiar el valor del input de la segunda semilla
+        inputIteraciones.value = ''; // Limpiar el valor del input de las iteraciones
+        resultado.textContent = ''; // Limpiar el contenido del resultado
+    }
+
+    btnReiniciar.addEventListener('click', reiniciarDatos);
+    
+    function obtenerDigitosCentrales(cadena) {
+        let longitud = cadena.length;
         let indiceInicio = Math.floor((longitud - 4) / 2);
         let indiceFin = Math.ceil((longitud + 4) / 2);
-    
-        // Extraer la subcadena con los cuatro dígitos centrales
         let centro = cadena.substr(indiceInicio, 4);
-    
+
         return centro;
     }
 });
